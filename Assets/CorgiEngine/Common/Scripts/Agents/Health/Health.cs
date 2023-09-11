e6dc900d726835afa990805a274962334b5f16df
김우찬 <woochankim0303@gmail.com>
2023-09-11 21:31:18 +0900

    LV-87 캐릭터 체력 UI

--- Assets/CorgiEngine/Common/Scripts/Agents/Health/Health.cs
+++ Assets/CorgiEngine/Common/Scripts/Agents/Health/Health.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using MoreMountains.Tools;
 using MoreMountains.Feedbacks;
+using UnityEngine.UI;
 
 namespace MoreMountains.CorgiEngine
 {
---
+++ Assets/Study/KWC/Scripts/Core/Agents/CustomHealth.cs
@@ -0,0 +1,19 @@
+﻿using MoreMountains.CorgiEngine;
+using MoreMountains.Tools;
+using UnityEngine.Serialization;
+
+public class CustomHealth : Health
+    {
+        [MMInspectorGroup("GUI", true, 12)]
+        public HealthBubble healthBubble;
+
+        public override void UpdateHealthBar(bool show)
+        {
+            base.UpdateHealthBar(show);
+        
+            if (healthBubble != null)
+            {
+                healthBubble.UpdateHeart((int)CurrentHealth, (int)MaximumHealth);
+            }
+        }
+    }
