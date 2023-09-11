41476405dfadb5427c164dc4f6bce5dcbb5fee15
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2023-09-11 09:48:40 +0900

    LV-66 점프대 수정완료

--- Assets/Study/CHW/Scripts/FallingBlock.cs
+++ Assets/Study/CHW/Scripts/FallingBlock.cs
@@ -4,15 +4,28 @@ using UnityEngine;
 
 public class FallingBlock : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] int _hp = 3;
+    [SerializeField] 
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	protected virtual void FixedUpdate()
+	{
+		// we send our various states to the animator.		
+		//UpdateAnimator();
+
+		//if (_timer < 0)
+		//{
+		//	_newPosition = new Vector2(0, -FallSpeed * Time.deltaTime);
+
+		//	transform.Translate(_newPosition, Space.World);
+
+		//	if (transform.position.y < _bounds.min.y)
+		//	{
+		//		DisableFallingPlatform();
+		//	}
+		//}
+	}
 }
