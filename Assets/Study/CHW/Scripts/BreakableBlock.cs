5d9f4cf404b003d46d672da744b4efd63cc664de
hyuncho <84334547+aquamagic9@users.noreply.github.com>
2023-09-11 13:30:33 +0900

    LV-64 화살에 반응하는 블록

---
+++ Assets/Study/CHW/Scripts/BreakableBlock.cs
@@ -0,0 +1,33 @@
+using MoreMountains.CorgiEngine;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BreakableBlock : MonoBehaviour
+{
+    public float delayBetweenDestruction = 1f;
+    public List<GameObject> BreakBlocks;
+    bool _active = false;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player") && this.GetComponent<Health>().CurrentHealth <= 0 && !_active)
+        {
+            _active = true;
+            StartDestructionSequence();
+        }
+    }
+    public void StartDestructionSequence()
+    {
+        StartCoroutine(DestroyBlocks());
+    }
+
+    private IEnumerator DestroyBlocks()
+    {
+        foreach (GameObject child in BreakBlocks)
+        {
+            Destroy(child.gameObject);
+            yield return new WaitForSeconds(delayBetweenDestruction);
+        }
+    }
+}
---
+++ Assets/Study/CHW/Scripts/Lever.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+
+public class Lever : MonoBehaviour
+{
+    public bool ActiveByWeapon = false;
+    public enum SwitchStates { On, Off }
+    public SwitchStates CurrentSwitchState { get; set; }
+    public SwitchStates InitialState = SwitchStates.Off;
+
+    public UnityEvent SwitchOn;
+    public UnityEvent SwitchOff;
+
+    private void Start()
+    {
+        CurrentSwitchState = InitialState;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player") && !ActiveByWeapon)
+        {
+            SetSwitchOn();
+        }
+        else if (collision.CompareTag("PlayerBullet") && ActiveByWeapon)
+        {
+            SetSwitchOn();
+        }
+    }
+
+    public void SetSwitchOn()
+    {
+        CurrentSwitchState = SwitchStates.On;
+        if (SwitchOn != null)
+        {
+            SwitchOn.Invoke();
+            this.gameObject.GetComponent<SpriteRenderer>().color = Color.red;
+        }
+    }
+}
